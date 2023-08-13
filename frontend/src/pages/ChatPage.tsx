46903b91b829318c7fc0e6a16583598bb0d0aa83
Akash Kumar Singh <akashviratsingh018@gmail.com>
2023-08-13 14:35:52 +0530

    connected: to the socket.io-client serever

--- frontend/src/pages/ChatPage.tsx
+++ frontend/src/pages/ChatPage.tsx
@@ -1,9 +1,13 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Box } from "@chakra-ui/react";
 import Sidebar from "../components/Sidebar";
 import ChatScreen from "../components/ChatScreen";
 import { Conversations } from "../assets/Data";
+import { io, Socket } from "socket.io-client";
 
+var BASEURL = import.meta.env.VITE_APP_URL;
+
+var socket: Socket;
 
 const ChatPage = () => {
   const [active, setActive] = useState<string>("Introductions");
@@ -16,7 +20,15 @@ const ChatPage = () => {
       id: 2,
       name: "General",
     },
+    {
+      id: 3,
+      name: "Office",
+    },
   ];
+  useEffect(() => {
+    console.log(BASEURL);
+    socket = io(BASEURL);  // Use the environment variable directly
+  }, []);
   return (
     <Box
       width={"100vw"}
